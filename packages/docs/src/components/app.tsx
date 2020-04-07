de3ae5397e1bb8621d755fdea0b0bbb71ded9b87
madou <laheen@gmail.com>
2020-04-08 07:21:50 +1000

    feat: adds migration page

--- packages/docs/src/components/app.tsx
+++ packages/docs/src/components/app.tsx
@@ -77,7 +77,7 @@ const components: MDXProviderComponentsProp = {
       </a>
     ) : (
       <Link
-        to={props.href}
+        to={href}
         css={{ color: '#7ab2c8', textDecoration: 'none' }}
         {...props}>
         {children}
