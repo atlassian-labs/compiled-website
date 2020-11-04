73186de71115a74f0f9c6128c508ddd3ac8146bb
Michael Dougall <mdougall@atlassian.com>
2020-11-04 13:19:30 +1100

    feat: adds slight animation to hero

--- packages/ui/src/components/hero.tsx
+++ packages/ui/src/components/hero.tsx
@@ -5,8 +5,28 @@ export const Hero = ({ children }: { children: React.ReactNode }) => {
   return (
     <div
       css={{
-        background: 'linear-gradient(120deg,#6554C0 0%, #FF7452 100%)',
+        position: 'relative',
+        overflow: 'hidden',
       }}>
+      <div
+        css={{
+          '@keyframes move': {
+            to: {
+              transform: 'translateX(25%)',
+            },
+          },
+          animation: '50s move',
+          animationFillMode: 'forwards',
+          background: 'linear-gradient(120deg,#6554C0 0%, #FF7452 100%)',
+          pointerEvents: 'none',
+          position: 'absolute',
+          top: 0,
+          left: '-50%',
+          right: 0,
+          bottom: 0,
+          zIndex: -1,
+        }}
+      />
       {children}
     </div>
   );
