c0a44bac02d4509be5a239eab86a907b18fdf2a4
Michael Dougall <mdougall@atlassian.com>
2020-11-10 14:01:17 +1100

    chore: affix the footer to the bottom of the viewport

--- packages/ui/src/components/footer.tsx
+++ packages/ui/src/components/footer.tsx
@@ -3,28 +3,35 @@ import '@compiled/core';
 import { Content, HorizontalStack } from '@compiled/website-ui';
 
 export const Footer = () => (
-  <footer
-    css={{
-      padding: '4rem',
-      fontSize: 11,
-      background: '#FAFBFC',
-      a: {
-        color: 'currentColor',
-        textDecoration: 'none',
-      },
-    }}>
-    <Content>
-      <HorizontalStack css={{ textAlign: 'center' }} gap={3}>
-        <a href="https://atlassian.com">
-          © {new Date().getFullYear()} Atlassian
-        </a>
-        <a href="https://www.atlassian.com/company/careers">Careers</a>
-        <a href="https://www.atlassian.com/legal/trademark">Trademark</a>
-        <a href="https://www.atlassian.com/legal/privacy-policy">Privacy</a>
-        <a href="https://github.com/atlassian-labs/compiled/blob/master/LICENSE">
-          License
-        </a>
-      </HorizontalStack>
-    </Content>
-  </footer>
+  <>
+    <div css={{ height: 80 }} />
+    <footer
+      css={{
+        padding: '4rem',
+        fontSize: 11,
+        background: '#FAFBFC',
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        a: {
+          color: 'currentColor',
+          textDecoration: 'none',
+        },
+      }}>
+      <Content>
+        <HorizontalStack css={{ textAlign: 'center' }} gap={3}>
+          <a href="https://atlassian.com">
+            © {new Date().getFullYear()} Atlassian
+          </a>
+          <a href="https://www.atlassian.com/company/careers">Careers</a>
+          <a href="https://www.atlassian.com/legal/trademark">Trademark</a>
+          <a href="https://www.atlassian.com/legal/privacy-policy">Privacy</a>
+          <a href="https://github.com/atlassian-labs/compiled/blob/master/LICENSE">
+            License
+          </a>
+        </HorizontalStack>
+      </Content>
+    </footer>
+  </>
 );
