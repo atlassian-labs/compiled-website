dbc777b08629503132c8ba544e52091b3c0a14a1
Michael Dougall <mdougall@atlassian.com>
2021-03-23 13:43:47 +1100

    chore: updates lozenge color

--- packages/ui/src/components/lozenge.tsx
+++ packages/ui/src/components/lozenge.tsx
@@ -1,17 +1,19 @@
 import * as React from 'react';
 import '@compiled/react';
 import { Text } from './text';
+import { primary, textLight } from '../utils/colors';
 
 export function Lozenge({ children }: { children: string }) {
   return (
     <span
       css={{
         display: 'inline-block',
-        backgroundColor: '#DEEBFF',
-        color: '#0747A6',
+        backgroundColor: primary,
+        color: textLight,
         padding: '4px 6px',
         borderRadius: 3,
         textTransform: 'uppercase',
+        lineHeight: 0,
       }}>
       <Text weight="bold" variant="supplementary">
         {children}
--- packages/ui/src/utils/colors.tsx
+++ packages/ui/src/utils/colors.tsx
@@ -3,3 +3,4 @@ export const heroBackground =
 export const headerBackground = '#091F41';
 export const codeBackground = 'rgb(39, 40, 34)';
 export const primary = '#8777D9';
+export const textLight = '#fff';
