3585af00d5cdda7b273e88f3089642d7475b49ab
madou <laheen@gmail.com>
2021-03-30 19:14:31 +1100

    feat: move to global css

--- packages/landing/src/index.tsx
+++ packages/landing/src/index.tsx
@@ -1,6 +1,7 @@
 import { render } from 'react-dom';
 import { StrictMode } from 'react';
 import LandingPage from './components/landing';
+import '@compiled/website-ui/global.css';
 
 render(
   <StrictMode>
