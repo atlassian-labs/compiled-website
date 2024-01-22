4f4bd273ff070998290b10cc90fdcb98e4e14655
Grant Wong <2908767+dddlr@users.noreply.github.com>
2024-01-23 12:09:34 +1300

    Update frontpage code examples (#925)
    * Update frontpage code examples
    
    * Update LargeText to have css call in function
    
    * Apply suggestions from code review
    
    Co-authored-by: Kylor Hall <136543114+kylorhall-atlassian@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Kylor Hall <136543114+kylorhall-atlassian@users.noreply.github.com>

--- packages/examples/src/class-names-button.tsx
+++ packages/examples/src/class-names-button.tsx
@@ -7,17 +7,19 @@ export const Button = ({ children }) => {
         children({
           className: css({
             border: 'none',
-            borderRadius: 3,
+            borderRadius: '3px',
             padding: '8px 10px',
             backgroundColor: '#6554C0',
             color: '#fff',
             fontWeight: 400,
-            fontFamily: 'arial',
-            fontSize: 14,
-            ':hover': {
+            fontFamily: 'Arial',
+            fontSize: '14px',
+
+            '&:hover': {
               backgroundColor: '#8777D9',
             },
-            ':active': {
+
+            '&:active': {
               backgroundColor: '#5243AA',
             },
           }),
--- packages/examples/src/styled-button.tsx
+++ packages/examples/src/styled-button.tsx
@@ -1,20 +1,20 @@
 import { styled } from '@compiled/react';
 
-export const Button = styled.button`
-  border: none;
-  border-radius: 3px;
-  padding: 8px 10px;
-  background-color: #6554c0;
-  color: #fff;
-  font-weight: 400;
-  font-family: arial;
-  font-size: 14px;
+export const Button = styled.button({
+  border: 'none',
+  borderRadius: '3px',
+  padding: '8px 10px',
+  backgroundColor: '#6554C0',
+  color: '#fff',
+  fontWeight: 400,
+  fontFamily: 'Arial',
+  fontSize: '14px',
 
-  :hover {
-    background-color: #8777d9;
-  }
+  '&:hover': {
+    backgroundColor: '#8777D9',
+  },
 
-  :active {
-    background-color: #5243aa;
-  }
-`;
+  '&:active': {
+    backgroundColor: '#5243AA',
+  },
+});
